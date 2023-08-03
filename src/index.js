655fa2213eb6e881d69b5c686ade4f8a19223510
Osiris <osifirsmafias99@gmail.com>
2023-08-03 16:08:51 -0600

    changed cors origin for deployment

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+// app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.use(cors());
 
 app.use(dishRouter);
 app.use(orderRouter);
